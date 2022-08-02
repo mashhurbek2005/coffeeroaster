abe0f9d6909d3ebe235bc6c4bce38c9a46d3a2a1
mashhurbek2005 <muhammedovmashhurbek@gmail.com>
2022-08-02 13:08:45 +0500

    Add site modal

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ elSiteHeaderToggleBtn.addEventListener("click", function(){
     elHeader.classList.toggle("site-header--open");
     elBody.classList.toggle("body--active");
 })
+
+//for modal
+var elModalOpenBtn = document.querySelector(".js-modal-open-btn");
+var elModal = document.querySelector(".modal");
+var elModalCloseBtn = document.querySelector(".js-modal-close-btn");
+
+elModalOpenBtn.addEventListener("click", function(){
+    elModal.classList.add("modal--show")
+})
+
+elModalCloseBtn.addEventListener("click", function(){
+    elModal.classList.remove("modal--show")
+})
